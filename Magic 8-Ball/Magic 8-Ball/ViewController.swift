8c7d10dd42927018e6e18f30093b02e29914c1a3
GSCrawley <gscrawley@gmail.com>
2021-02-10 17:09:23 -0800

    Magic 8 Ball storyboard ready to go

---
+++ Magic 8-Ball/Magic 8-Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic 8-Ball
+//
+//  Created by Gideon S Crawley on 2/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
